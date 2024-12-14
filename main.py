cfc41b49346a9d31e034a20a6cd33a6980249974
kpk <kpk@abstractit.in>
2024-12-14 11:18:13 +0530

    Deleted output file.

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ response = requests.post(API_URL, headers=headers, json=payload)
 # Handle the response
 if response.status_code == 200:
     # Save the audio file
-    with open("generated_audio.wav", "wb") as audio_file:
+    with open(".generated_audio.wav", "wb") as audio_file:
         audio_file.write(response.content)
     print("Audio generated and saved as 'generated_audio.wav'")
 else:
